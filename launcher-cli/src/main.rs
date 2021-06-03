f8fd3469f61da59e6100addd8213ad953eddcb12
TheOddGarlic <umutinanerdogan62@gmail.com>
2021-06-03 19:01:30 +0300

    Use anyhow in launcher-cli too.

--- launcher-cli/src/main.rs
+++ launcher-cli/src/main.rs
@@ -3,6 +3,7 @@
  * Licensed under the Open Software License version 3.0
  */
 
+use anyhow::Result;
 use auth::yggdrasil::Authenticate;
 use clap::{App, Arg};
 use launcher_core::Launcher;
@@ -10,8 +11,8 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::{env, fs, io};
 
-fn main() {
-    let default_launcher_directory = &launcher_directory();
+fn main() -> Result<()> {
+    let default_launcher_directory = launcher_directory()?;
 
     let matches = App::new("launcher-cli")
         .version("0.1")
@@ -23,7 +24,7 @@ fn main() {
                 .long("launcher_directory")
                 .value_name("LAUNCHER_DIRECTORY")
                 .help("Sets a directory for the launcher and game to be run in.")
-                .default_value(default_launcher_directory)
+                .default_value(&default_launcher_directory)
                 .takes_value(true),
         )
         .get_matches();
@@ -31,11 +32,11 @@ fn main() {
     let launcher_directory = PathBuf::from(
         matches
             .value_of("LAUNCHER_DIRECTORY")
-            .unwrap_or(default_launcher_directory),
+            .unwrap_or(&default_launcher_directory),
     );
 
-    fs::create_dir_all(&launcher_directory).unwrap();
-    env::set_current_dir(&launcher_directory).unwrap();
+    fs::create_dir_all(&launcher_directory)?;
+    env::set_current_dir(&launcher_directory)?;
 
     // TODO: save auth_response to file and load it if file exists,
     //       validate if the credentials are still valid and
@@ -44,20 +45,18 @@ fn main() {
     let mut password = String::new();
 
     print!("Email or username: ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
     io::stdin()
         .read_line(&mut email_or_username)
         .expect("Reading email or username failed.");
 
     print!("Password: ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
     io::stdin()
         .read_line(&mut password)
         .expect("Reading password failed.");
 
-    let auth_response = Authenticate::new(email_or_username.trim(), password.trim())
-        .perform()
-        .unwrap();
+    let auth_response = Authenticate::new(email_or_username.trim(), password.trim()).perform()?;
 
     let launcher = Launcher::builder(&auth_response)
         .game_directory(launcher_directory)
@@ -65,20 +64,24 @@ fn main() {
 
     // TODO: download minecraft
     // TODO: launch minecraft
+
+    Ok(())
 }
 
-fn launcher_directory() -> String {
-    env::var("MC_LAUNCHER_DIRECTORY").unwrap_or(if cfg!(target_os = "windows") {
-        format!("{}\\.minecraft-cli-launcher", env::var("APPDATA").unwrap())
-    } else if cfg!(target_os = "macos") {
-        format!(
-            "{}/Library/Application Support/.minecraft-cli-launcher",
-            env::var("HOME").unwrap()
-        )
-    } else {
-        format!(
-            "{}/.minecraft-cli-launcher",
-            env::var("XDG_CONFIG_HOME").unwrap_or(env::var("HOME").unwrap())
-        )
-    })
+fn launcher_directory() -> Result<String> {
+    Ok(
+        env::var("MC_LAUNCHER_DIRECTORY").unwrap_or(if cfg!(target_os = "windows") {
+            format!("{}\\.minecraft-cli-launcher", env::var("APPDATA").unwrap())
+        } else if cfg!(target_os = "macos") {
+            format!(
+                "{}/Library/Application Support/.minecraft-cli-launcher",
+                env::var("HOME").unwrap()
+            )
+        } else {
+            format!(
+                "{}/.minecraft-cli-launcher",
+                env::var("XDG_CONFIG_HOME").unwrap_or(env::var("HOME").unwrap())
+            )
+        }),
+    )
 }
